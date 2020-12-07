2e58b0fb8cf55567dd7670e4c0805ccb832f1d51
laurent <wrmultitudes@gmail.com>
2020-12-07 19:01:10 +0100

    day 7

--- Pages/Day7.xcplaygroundpage/Contents.swift
+++ Pages/Day7.xcplaygroundpage/Contents.swift
@@ -8,71 +8,67 @@ import Foundation
 
 //guard let url = Bundle.main.url(forResource: "day7-example", withExtension: "txt") else {fatalError()}
 guard let url = Bundle.main.url(forResource: "input", withExtension: "txt") else {fatalError()}
+//guard let url = Bundle.main.url(forResource: "day7-Part2-example", withExtension: "txt") else {fatalError()}
+
 guard let input = try? String(contentsOf: url) else {fatalError()}
 
 var rules = input.lines.compactMap {$0.trimmingCharacters(in: .punctuationCharacters)}
 
-var rulesDict: [String: [String]] = [:]
+var rulesDict: [String: [(numberOfBags:Int, bagType: String)]] = [:]
 rules.forEach {
 	if let groups = $0.getCapturedGroupsFrom(regexPattern:"([a-z ]+)bags? contain (.+)") {
 		let bagRule = groups[0].trimmingCharacters(in: .whitespaces)
 		let contents = groups[1].split(separator: ",").map {String($0).trimmingCharacters(in: .whitespaces)}
-		//print("contents ", contents)
 		for content in contents {
-			//print("contentof contents ----------",bagRule,content)
 			if let data = content.getCapturedGroupsFrom(regexPattern:"(\\d) (\\w+ \\w+) bags?") {
-
-				let arrayBags = [data[1]]
-				//print(arrayBags)
-				rulesDict[bagRule, default: []] += arrayBags
-				//print(rulesDict.debugDescription)
+				let dataTuple: (numberOfBags:Int, bagType: String) = (numberOfBags: Int(data[0]) ?? 0, bagType: data[1])
+				rulesDict[bagRule, default: []] += [dataTuple]
 			}
-			if let _ = content.getCapturedGroupsFrom(regexPattern:"(no other) bags") {
-				rulesDict[bagRule, default: []] = []
-				//print(data)
-			}
-
 		}
 	}
 }
 
 var cache = [String:Bool]()
-func containsRecursively(bag: String, in dict: [String:[String]]) -> Bool {
-	if bag == "shiny gold" {return true}
+func containsRecursively(bag: String, in dict: [String: [(numberOfBags: Int, bagType: String)]]) -> Bool {
 
-	if let res = cache[bag] {print("\ncached!! \(bag)\n");return res}
-	let bags = dict[bag] ?? []
+	if bag == "shiny gold" {return true}
+	if let result = cache[bag] {return result}
+	let bags = dict[bag, default: []]
 
 	for bag in bags {
-		//print("recursion =======", bag)
-		if containsRecursively(bag: bag, in: dict) == true {
+		if containsRecursively(bag: bag.bagType, in: dict) == true {
 			return true
 		}
-		cache[bag] = false
+		cache[bag.bagType] = false
 	}
 	return false
 }
 
-//var a = containsRecursively(bag: "muted yellow", in: rulesDict)
-//var a = containsRecursively(bag: "faded blue", in: rulesDict)
-//print(a)
-//print(rulesDict["faded blue"]?.contains("shiny gold")  )
-//rulesDict["faded blue"]
-//print(rulesDict.keys, "\n")
-//rulesDict["bright white"]
-//a = containsRecursively(bag: "bright white", in: rulesDict)
-//rulesDict["dark orange"]
-//a = containsRecursively(bag: "dark orange", in: rulesDict)
-//a = containsRecursively(bag: "light red", in: rulesDict)
-
 var solution: [String] = []
-//rulesDict["shiny gold"] = nil
 for key in rulesDict.keys {
 	if key == "shiny gold" { continue }
-	//print("key", key, rulesDict[key]!)
 	if containsRecursively(bag: key, in: rulesDict) == true {
 		solution.append(key)
 	}
 }
 print(solution)
-print(solution.count)
+print(solution.count) // 161
+
+var shinyCache = [String:Int]()
+
+func countBagsInside(for bag: String, in dict: [String: [(numberOfBags: Int, bagType: String)]]) -> Int {
+	if let cached = shinyCache[bag] {
+		return cached }
+	let contentsOfBag = dict[bag, default: []]
+	if contentsOfBag.isEmpty { return 0 }
+	let total = contentsOfBag.reduce(0) {subTotal, element in
+		subTotal + element.numberOfBags + (element.numberOfBags * countBagsInside(for: element.bagType, in: dict))
+	}
+	shinyCache[bag] = total
+	return total
+}
+
+let count = countBagsInside(for: "shiny gold", in: rulesDict)
+print(count)
+//30899
+
