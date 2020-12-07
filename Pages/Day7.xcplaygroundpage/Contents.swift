5ad2e220def8d2d1aa3c76043d04ac60ba100246
laurent <wrmultitudes@gmail.com>
2020-12-07 17:23:43 +0100

    day7

--- Pages/Day7.xcplaygroundpage/Contents.swift
+++ Pages/Day7.xcplaygroundpage/Contents.swift
@@ -6,31 +6,29 @@ import Foundation
 
 //	--- Day 7: Handy Haversacks ---
 
-guard let url = Bundle.main.url(forResource: "day7-example", withExtension: "txt") else {fatalError()}
+//guard let url = Bundle.main.url(forResource: "day7-example", withExtension: "txt") else {fatalError()}
+guard let url = Bundle.main.url(forResource: "input", withExtension: "txt") else {fatalError()}
 guard let input = try? String(contentsOf: url) else {fatalError()}
 
-//print(input)
-
 var rules = input.lines.compactMap {$0.trimmingCharacters(in: .punctuationCharacters)}
-//.split(separator: " ").split {$0 == "contain"} }
 
 var rulesDict: [String: [String]] = [:]
 rules.forEach {
 	if let groups = $0.getCapturedGroupsFrom(regexPattern:"([a-z ]+)bags? contain (.+)") {
 		let bagRule = groups[0].trimmingCharacters(in: .whitespaces)
 		let contents = groups[1].split(separator: ",").map {String($0).trimmingCharacters(in: .whitespaces)}
-
+		//print("contents ", contents)
 		for content in contents {
-			print("contentof contents ----------",bagRule,content)
+			//print("contentof contents ----------",bagRule,content)
 			if let data = content.getCapturedGroupsFrom(regexPattern:"(\\d) (\\w+ \\w+) bags?") {
 
-				let arrayBags = Array(repeating: data[1], count: Int(data[0]) ?? 0)
+				let arrayBags = [data[1]]
 				//print(arrayBags)
 				rulesDict[bagRule, default: []] += arrayBags
 				//print(rulesDict.debugDescription)
 			}
 			if let _ = content.getCapturedGroupsFrom(regexPattern:"(no other) bags") {
-				rulesDict[bagRule, default: []] += ["no other"]
+				rulesDict[bagRule, default: []] = []
 				//print(data)
 			}
 
@@ -38,35 +36,43 @@ rules.forEach {
 	}
 }
 
-//func containsRecursively(bag: String, in dict: [String:[String]]) -> Bool {
-//	let bags = dict[bag] ?? ["No color"]
-//	if bags.count == 1 {
-//		if bags.contains("shiny gold") {
-//			print("ok\n")
-//			return true
-//		}
-//	} else { return false }
-//
-//	//return dict[bag]?.map {  containsRecursively(bag: $0, in: dict) }
-//	for bag in bags {
-//		containsRecursively(bag: bag, in: dict)
-//	}
-//	return false
-//}
+var cache = [String:Bool]()
+func containsRecursively(bag: String, in dict: [String:[String]]) -> Bool {
+	if bag == "shiny gold" {return true}
+
+	if let res = cache[bag] {print("\ncached!! \(bag)\n");return res}
+	let bags = dict[bag] ?? []
+
+	for bag in bags {
+		//print("recursion =======", bag)
+		if containsRecursively(bag: bag, in: dict) == true {
+			return true
+		}
+		cache[bag] = false
+	}
+	return false
+}
 
 //var a = containsRecursively(bag: "muted yellow", in: rulesDict)
+//var a = containsRecursively(bag: "faded blue", in: rulesDict)
+//print(a)
+//print(rulesDict["faded blue"]?.contains("shiny gold")  )
+//rulesDict["faded blue"]
+//print(rulesDict.keys, "\n")
+//rulesDict["bright white"]
 //a = containsRecursively(bag: "bright white", in: rulesDict)
+//rulesDict["dark orange"]
 //a = containsRecursively(bag: "dark orange", in: rulesDict)
 //a = containsRecursively(bag: "light red", in: rulesDict)
-//var a = containsRecursively(bag: "faded blue", in: rulesDict)
-print(rulesDict["faded blue"]?.contains("shiny gold")  )
-print(rulesDict.keys, "\n")
-//var solution: [String] = []
-//for key in rulesDict.keys {
-//	print("key", key, rulesDict[key]!)
-//	if containsRecursively(bag: key, in: rulesDict) == true {
-//		solution.append(key)
-//	}
-//}
 
-//print(solution)
+var solution: [String] = []
+//rulesDict["shiny gold"] = nil
+for key in rulesDict.keys {
+	if key == "shiny gold" { continue }
+	//print("key", key, rulesDict[key]!)
+	if containsRecursively(bag: key, in: rulesDict) == true {
+		solution.append(key)
+	}
+}
+print(solution)
+print(solution.count)
