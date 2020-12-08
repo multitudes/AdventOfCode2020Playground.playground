5c973b3b3651de983d7a51c636d7d9f1d0ee29d9
laurent <wrmultitudes@gmail.com>
2020-12-08 17:17:54 +0100

    Update Contents.swift

--- Pages/Day8.xcplaygroundpage/Contents.swift
+++ Pages/Day8.xcplaygroundpage/Contents.swift
@@ -11,9 +11,9 @@ guard let url = Bundle.main.url(forResource: "input", withExtension: "txt") else
 //guard let url = Bundle.main.url(forResource: "day8-example", withExtension: "txt") else {fatalError()}
 guard let input = try? String(contentsOf: url).lines else {fatalError()}
 let instructions: [(operation: Operation, argument: Int)] = input.compactMap { line in
-	if let instructionTuple = line.getCapturedGroupsFrom(regexPattern: "^(\\w{3}) ([+|-]\\d+)") {
-		if let operation = Operation(instructionTuple[0]) {
-			return (operation: operation, argument: Int(instructionTuple[1]) ?? 0)
+	if let capturedGroups = line.getCapturedGroupsFrom(regexPattern: "^(\\w{3}) ([+|-]\\d+)") {
+		if let operation = Operation(capturedGroups[0]) {
+			return (operation: operation, argument: Int(capturedGroups[1]) ?? 0)
 		}}
 	return nil
 }
@@ -26,16 +26,15 @@ enum Operation: String, CustomStringConvertible {
 	}
 	var description : String { return self.rawValue }
 }
-var visited: [Int: (operation: Operation, argument: Int)] = [:]
-//var visited: [Int: Bool] = [:]
+//var visited: [Int: (operation: Operation, argument: Int)] = [:]
+var visited: [Int: Bool] = [:]
 var counter: Int = 0
 var accumulator: Int = 0
 while true {
 	if visited[counter] != nil {
-		print("Solution part 1: ",accumulator) //1394
 		break
 	}
-	visited[counter] = (operation: instructions[counter].operation , argument: instructions[counter].argument )
+	visited[counter] = true
 	switch instructions[counter].operation {
 		case .nop:
 			print("nop")
@@ -50,57 +49,46 @@ while true {
 	}
 	print("counter", counter, "acc", accumulator)
 }
-print(visited.debugDescription)
+print("Solution part 1: ",accumulator) //1394
+
+//print(visited.debugDescription)
 
 // --- part two ---
 
-//outerloop : for key in visited.keys.sorted() {
 outerloop : for (key,_)  in instructions.enumerated() {
-	print("key", key)
 	// make a copy of  instructions
 	var instructionsCopy: [(operation: Operation, argument: Int)] = instructions
 	// change one key!
 	let operation = instructionsCopy[key].operation
 	switch operation {
 		case .nop:
-			print("changed nop to jmp")
 			instructionsCopy[key].operation = .jmp
 		case .jmp:
-			print("changing jmp to nop", instructionsCopy[key])
 			instructionsCopy[key].operation = .nop
-			print("changed jmp to nop", instructionsCopy[key])
-
 		default:
 			continue
 	}
-
-	// reset accumulator and counter
 	accumulator = 0
 	counter = 0
-
-	var visitedPart2: [Int: Bool] = [:]
+	visited = [:]
 	while true {
-		if counter == instructions.count  { print("solution2: \(accumulator)");break outerloop }
+		if counter == instructions.count  { break outerloop }
 		if counter >= instructions.count + 1 { print("out of bounds\n\n\n"); continue outerloop }
-		if visitedPart2[counter] == true {
-			print("break",accumulator, key)
+		if visited[counter] == true {
 			break
 		}
-		visitedPart2[counter] = true
+		visited[counter] = true
 		switch instructionsCopy[counter].operation {
 			case .nop:
-				print("nop")
 				counter += 1
 			case .acc:
-				print("acc")
 				accumulator += instructionsCopy[counter].argument
 				counter += 1
 			case .jmp:
-				print("jmp")
 				counter += instructionsCopy[counter].argument
 		}
 		print("counter", counter, "acc", accumulator)
-		//print(instructionsCopy)
 	}
 }
+print("solution2: \(accumulator)")
 print("end")
