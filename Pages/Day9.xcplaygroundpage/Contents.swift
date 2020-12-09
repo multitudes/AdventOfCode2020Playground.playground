7afe6ecf5ca9dbb71336a40119d04aa99b356346
laurent <wrmultitudes@gmail.com>
2020-12-09 15:41:58 +0100

    Update Contents.swift

--- Pages/Day9.xcplaygroundpage/Contents.swift
+++ Pages/Day9.xcplaygroundpage/Contents.swift
@@ -7,30 +7,16 @@ import Foundation
 
 guard let url = Bundle.main.url(forResource: "input", withExtension: "txt") else {fatalError()}
 //guard let url = Bundle.main.url(forResource: "day9-example", withExtension: "txt") else {fatalError()}
-
 guard let inputLines = try? String(contentsOf: url).lines.compactMap(Int.init) else {fatalError()}
 
 var input = inputLines
-let preamble = 25
-var workingQueue = input.prefix(preamble)
-var runningIndex = preamble
-let count = input.count
-input
-var solution1 = 0
-var solution2 = 0
-
+let preamble = 25; var workingQueue = input.prefix(preamble)
+var runningIndex = preamble; let count = input.count
 while runningIndex < count {
 	let next = input[runningIndex]
-	print("workingQueue", workingQueue, " next ", next)
-
-	if checksums(queue: workingQueue, with: next).isValid { // (true, nil)
-		print("ok")
-	} else {
-		print("ok", next, ">>>>>>>>>>>>>>>>>>>>>>>>")
-		solution1 = next
-		if let solution = checkForRange(with: next)  { print("Solution2 ! ")
-			solution2 = solution
-		}
+	if !checksums(queue: workingQueue, with: next).isValid {
+		print("Solution part 1: ", next) //18272118
+		print("Solution part 2: ", checkForRange(with: next) ?? 0) // 2186361
 		break
 	}
 	workingQueue = workingQueue.dropFirst()
@@ -38,28 +24,17 @@ while runningIndex < count {
 	runningIndex += 1
 }
 
-print("Solution part 1: ", solution1) //18272118
-print("Solution part 2: ", solution2) // 2186361
-
-
 func checkForRange(with invalidNumber: Int) -> Int? {
 	for i in 0..<count {
-		var partialSum = 0
+		var partialSum = 0; runningIndex = i
 		var subSequence: Array<Int>.SubSequence = []
-		runningIndex = i
-		if input[runningIndex] + input[runningIndex + 1] == invalidNumber {
-			return input[runningIndex] + input[runningIndex + 1]
-		}
-		if input[runningIndex] + input[runningIndex + 1] > invalidNumber {
-			continue
-		}
 		while runningIndex < count - 1 {
 			let currentNumber = input[runningIndex]
 			runningIndex += 1; partialSum += currentNumber
 			subSequence.append(currentNumber)
-			if partialSum >  invalidNumber { break	}
+			if partialSum >  invalidNumber {break}
 			if partialSum == invalidNumber {
-				if subSequence.count == 1 { continue }
+				if subSequence.count == 1 {continue}
 				return subSequence.sorted().first! + subSequence.sorted().last!
 			}
 		}
@@ -67,35 +42,23 @@ func checkForRange(with invalidNumber: Int) -> Int? {
 	return nil
 }
 
-
-
 //check the workingQ is valid!
 func checksums(queue: Array<Int>.SubSequence, with next: Int) -> (isValid:Bool, solution: Int? ) {
-	let q = queue.sorted()
-	print("working queue sorted ", q)
-	let lastIndex = q.count - 1
+	let q = queue.sorted();	let lastIndex = q.count - 1
 	var first:Int = 0; var last: Int = lastIndex
-	let lasttwosum = q[lastIndex] + q[lastIndex-1]
-	if lasttwosum < next {
-		print("Error // solution is ",next); return (false, next) }
-
+	if q[lastIndex] + q[lastIndex-1] < next {return (false, next)}
 	while first < last {
 		let sum = q[first] + q[last]
 		switch sum  {
-			case next :
-				print("q is ok /// ")
-				return (true, nil)
-			case (..<next) :
-				first += 1
-				continue
-			default:
-				last -= 1
-				continue
+			case next : return (true, nil)
+			case (..<next): first += 1;	continue
+			default: last -= 1;	continue
 		}
 	}
-	print("Error // solution is ",next)
 	return (false, next)
 }
+
+
 
 
 
