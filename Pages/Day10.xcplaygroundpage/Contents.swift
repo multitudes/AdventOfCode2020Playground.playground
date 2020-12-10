a2d1a2843ab6febc09b305091d4612b858f3c642
laurent <wrmultitudes@gmail.com>
2020-12-10 22:26:48 +0100

    day10

--- Pages/Day10.xcplaygroundpage/Contents.swift
+++ Pages/Day10.xcplaygroundpage/Contents.swift
@@ -6,49 +6,48 @@ var str = "Hello, playground"
 
 //: [Next](@next)
 
-enum AdventError: Error {
-	case fileNotFound, fileNotValid
-}
+enum AdventError: Error {case fileNotFound, fileNotValid}
 
-infix operator ^^
-func ^^<I: BinaryInteger>(lhs: I, rhs: I) -> I {
-	return I(pow(Double(lhs), Double(rhs)))
-}
-// --- Day 10: Adapter Array ---
-
-//guard let url = Bundle.main.url(forResource: "input", withExtension: "txt") else {fatalError()}
-guard let url = Bundle.main.url(forResource: "day10-example2", withExtension: "txt") else {fatalError()}
-guard let input = try? String(contentsOf: url).lines.compactMap(Int.init).sorted() else {fatalError()}
-
-//var jolts: [Int: Int] = [:]
-//var previousOutput = 0
-//for adapter in input.enumerated() {
-//	let joltage = adapter-previousOutput
-//	previousOutput = adapter
-//	jolts[joltage, default: 0] += 1
-//}
-//
-//jolts[3, default: 0] += 1
-//let jolts3 = jolts[3, default: 0]
-//let jolts1 = jolts[1, default: 0]
-//print(jolts.description)
-//
-//let solution = jolts1 * jolts3 //1904
-
-
-// here recursion for every element in the working q
-// -> check the range - if one of them is the last element or workking q isempty
-// add one to the counter of sequences
-var currentAdapter = 0
-func checkAdaptersInRange(with currentAdapter: Int) -> Int {
-	if currentAdapter == input.last! { return 1 }
-	var validSequences = 0
-	for i in Range(1...3) {
-		if input.contains(currentAdapter + i) {
-			validSequences += checkAdaptersInRange(with: currentAdapter + i)
-		}
+struct FileLinesIterator: IteratorProtocol {
+	let lines: [Int]
+	var currentLine: Int = 0
+	init(filename: String) throws {
+		guard let url = Bundle.main.url(forResource: filename, withExtension: nil) else {throw AdventError.fileNotFound}
+		let contents: String = try String(contentsOf: url)
+		lines = contents.lines.compactMap(Int.init).sorted()
 	}
-	return validSequences
+	mutating func next() -> Int? {
+		guard currentLine < lines.endIndex else { return nil }
+		defer {currentLine += 1}
+		return lines[currentLine]
+	}
 }
 
-let solution = checkAdaptersInRange(with: currentAdapter)
+var input = try? FileLinesIterator(filename: "input.txt")
+var accumulator = 1
+var partial: Int? = nil
+var jolts: [Int: Int] = [:]
+var previousOutput = 0
+while true {
+	if let adapter = input?.next() {
+		let joltage = adapter-previousOutput
+		previousOutput = adapter
+		jolts[joltage, default: 0] += 1
+		// part 2
+		if joltage == 3 {accumulator *= partial ?? 1; partial = nil}
+		if joltage == 1 {
+			if partial == nil {partial = 1; continue}
+			if partial == 1 {partial = 2; continue}
+			if partial! == 2 {partial! += 2; continue}
+			if partial! == 4 {partial! += 3; continue}
+		}
+		continue
+	}
+	jolts[3, default: 0] += 1
+	let jolts3 = jolts[3, default: 0]
+	let jolts1 = jolts[1, default: 0]
+
+	let solution = jolts1 * jolts3 //1904
+	let solution2 = accumulator * (partial ?? 1) //10578455953408
+	break
+}
---
+++ Pages/Day11.xcplaygroundpage/Contents.swift
@@ -0,0 +1,27 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var str = "Hello, playground"
+
+//: [Next](@next)
+
+//enum MyEnum: String {
+//	case foo, bar
+//}
+//
+//var dict = [String: Set<MyEnum>]()
+//var myEnum = MyEnum.foo
+//var myEnumBar = MyEnum.bar
+//dict["a"] = [myEnum]
+//
+//print(dict.description)
+//
+//if var enums = dict["a"] {
+//	enums.insert(myEnumBar)
+//}
+//print(dict)
+
+
+
+
