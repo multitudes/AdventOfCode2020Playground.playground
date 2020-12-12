d78ef7d53aaa481232f006bb78119649a371e6d8
laurent <wrmultitudes@gmail.com>
2020-12-12 16:15:44 +0100

    Update Contents.swift

--- Pages/Day12.xcplaygroundpage/Contents.swift
+++ Pages/Day12.xcplaygroundpage/Contents.swift
@@ -3,88 +3,105 @@ import Foundation
 
 //	--- Day 12: Rain Risk ---
 
-guard let url = Bundle.main.url(forResource: "day12-example", withExtension: "txt") else { fatalError() }
-//guard let url = Bundle.main.url(forResource: "input", withExtension: "txt") else { fatalError() }
+//guard let url = Bundle.main.url(forResource: "day12-example", withExtension: "txt") else { fatalError() }
+guard let url = Bundle.main.url(forResource: "input", withExtension: "txt") else { fatalError() }
 guard let input: [String] = try? String(contentsOf: url).components(separatedBy: .whitespacesAndNewlines).dropLast() else {fatalError()}
 
-var trajectory: [(action: Character, param: Int)] = input.map {
+var trajectory: [(action: Action, param: Int)] = input.map {
 	let instruction = Array($0)
+	let action = instruction.first!
 	let parameter = instruction[1...]
-	return (action: instruction.first!, param: Int(String(parameter))! ) }
+	return (action: Action(rawValue: action)!, param: Int(String(parameter))! )
+}
+
+enum Action: Character {
+	case forward = "F"; case north = "N"; case south = "S"; case east = "E"; case west = "W"
+	case turnRight = "R"; case turnLeft = "L";
+}
 
 enum Direction: Int, CaseIterable {
 	case east, south, west, north
-	static var aHead = Direction.east
-	static func turn(_ turning: String, degrees: Int){
-		if turning == "left" {
-			if let currentDirection = allCases.firstIndex(of: aHead) {
-				let idx = (currentDirection + allCases.count - (degrees / 90)) % allCases.count
-				aHead = Direction(rawValue: idx)!
-				print(aHead)
-			}
+	static var facing = Direction.east
+	static func turnLeft(_ degrees: Int){
+		if let currentDirection = allCases.firstIndex(of: facing) {
+			let idx = (currentDirection + allCases.count - (degrees / 90)) % allCases.count
+			facing = Direction(rawValue: idx)!
+			//print(facing)
+		}}
+	static func turnRight(_ degrees: Int){
+		if let currentDirection = allCases.firstIndex(of: facing) {
+			let idx = (currentDirection + allCases.count + (degrees / 90)) % allCases.count
+			facing = Direction(rawValue: idx)!
+			//print(facing)
 		}
-		if turning == "right" {
-			if let currentDirection = allCases.firstIndex(of: aHead) {
-				let idx = (currentDirection + allCases.count + (degrees / 90)) % allCases.count
-				aHead = Direction(rawValue: idx)!
-				print(aHead)
-			}}
 	}
 }
+
+func move(_ direction: Direction, amount: Int) {
+	switch Direction.facing {
+		case .north:
+			position.y += amount
+		case .south:
+			position.y -= amount
+		case .east:
+			position.x += amount
+		case .west:
+			position.x -= amount
+	}
+}
+
+func decode(_ action: Action, amount: Int) {
+	switch action {
+		case .forward :
+			// move forward
+			move(Direction.facing, amount: amount)
+		case .west:
+			position.x -= amount
+		case .south:
+			position.y -= amount
+		case .north:
+			position.y += amount
+		case .east:
+			position.x += amount
+		case .turnRight:
+			Direction.turnRight(amount)
+		case .turnLeft:
+			Direction.turnLeft(amount)
+	}
+}
+
 
 var position: (x: Int, y: Int) = (0,0)
 
-func move(_ action: Character, amount: Int) {
-	switch action {
-		case "F":
-			// move forward by action
-			switch Direction.aHead {
-				case .north:
-					position.y += amount
-				case .south:
-					position.y -= amount
-				case .east:
-					position.x += amount
-				case .west:
-					position.x -= amount
-			}
-		case "W":
-			position.x -= amount
-		case "S":
-			position.y -= amount
-		case "N":
-			position.y += amount
-		case "E":
-			position.x += amount
-		default:
-			<#code#>
-	}
+for instruction in trajectory {
+	decode(instruction.action, amount: instruction.param)
 }
+print(position)
+let manhattanDistance = abs(position.x) + abs(position.y)
 
+//
+//
+//
+//print(Direction.allCases)
+//Direction.turnLeft(90)
+//Direction.turnRight(90)
 
-
-print(Direction.allCases)
-Direction.turn("left", degrees: 90)
-Direction.turn("right", degrees: 90)
-
-var directionToHead = Direction.east
-Direction.aHead = .south
-switch Direction.aHead {
-	case .north:
-		print("Lots of planets have a north")
-	case .south:
-		print("Watch out for penguins")
-	case .east:
-		print("Where the sun rises")
-	case .west:
-		print("Where the skies are blue")
-}
-
+//var directionToHead = Direction.east
+//Direction.facing = .south
+//switch Direction.facing {
+//	case .north:
+//		print("Lots of planets have a north")
+//	case .south:
+//		print("Watch out for penguins")
+//	case .east:
+//		print("Where the sun rises")
+//	case .west:
+//		print("Where the skies are blue")
+//}
+//
 
 
 //print("Solution part 1: ", solution1) //
 //print("Solution part 2: ", solution2) //
 
-enum Action: Character {
-	case forward = "F"
-}
+
