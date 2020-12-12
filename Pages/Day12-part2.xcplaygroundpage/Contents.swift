41593e836e761c31df0544d7f014e1c3c259c269
laurent <wrmultitudes@gmail.com>
2020-12-12 18:38:03 +0100

    day12

--- Pages/Day12-part2.xcplaygroundpage/Contents.swift
+++ Pages/Day12-part2.xcplaygroundpage/Contents.swift
@@ -19,51 +19,42 @@ enum Action: Character {
 	case turnRight = "R"; case turnLeft = "L";
 }
 
-enum Direction: Int, CaseIterable {
-	case east, south, west, north
-	static var facing = Direction.east
-	static func turn(_ degrees: Int){
-		if let currentDirection = allCases.firstIndex(of: facing) {
-			let idx = (currentDirection + allCases.count + (degrees / 90)) % allCases.count
-			facing = Direction(rawValue: idx)!
-		}
+let antiClockwiseVector: (x: Int, y: Int) = (x:-1, y: 1)
+let ClockwiseVector: (x: Int, y: Int) = (x:1, y: -1)
+
+func turn(_ degrees: Int, direction vector: (x: Int, y: Int)){
+	let times = degrees / 90
+	for _ in 0..<times {
+		 (waypoint.y, waypoint.x) = (waypoint.x, waypoint.y)
+		waypoint.x *= vector.x; waypoint.y *= vector.y;
 	}
 }
 
-func moveShip(amount: Int) {
-	switch Direction.facing {
-		case .north:
-			position.y += amount
-		case .south:
-			position.y -= amount
-		case .east:
-			position.x += amount
-		case .west:
-			position.x -= amount
-	}
-}
-
-func run(_ action: Action, amount: Int) {
+func runPartTwo(_ action: Action, amount: Int) {
 	switch action {
 		case .forward :
-			moveShip(amount: amount)
+			position.x += waypoint.x * amount
+			position.y += waypoint.y * amount
 		case .west:
-			position.x -= amount
+			waypoint.x -= amount
 		case .south:
-			position.y -= amount
+			waypoint.y -= amount
 		case .north:
-			position.y += amount
+			waypoint.y += amount
 		case .east:
-			position.x += amount
+			waypoint.x += amount
 		case .turnRight:
-			Direction.turn(amount)
+			turn(amount, direction: ClockwiseVector)
 		case .turnLeft:
-			Direction.turn(-amount)
+			turn(amount, direction: antiClockwiseVector)
 	}
 }
 
+// starting values
 var position: (x: Int, y: Int) = (0,0)
+var waypoint: (x: Int, y: Int) = (10,1)
 
-trajectory.map {run($0.action, amount: $0.param)}
+trajectory.map { runPartTwo($0.action, amount: $0.param )}
 
-print("Solution part 1: ", abs(position.x) + abs(position.y)) //2057
+print("Solution part 2: ", abs(position.x) + abs(position.y)) //71504
+
