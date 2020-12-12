6d5430e42b95af3b4e1ee12f139833bdfa43d518
laurent <wrmultitudes@gmail.com>
2020-12-12 15:02:38 +0100

    day12

--- Pages/Day12.xcplaygroundpage/Contents.swift
+++ Pages/Day12.xcplaygroundpage/Contents.swift
@@ -1,106 +1,57 @@
 import Foundation
 
 
-//	--- Day 11:  ---
+//	--- Day 12: Rain Risk ---
 
-guard let url = Bundle.main.url(forResource: "day11-example", withExtension: "txt") else { fatalError() }
+guard let url = Bundle.main.url(forResource: "day12-example", withExtension: "txt") else { fatalError() }
 //guard let url = Bundle.main.url(forResource: "input", withExtension: "txt") else { fatalError() }
-guard let input: [String] = try? String(contentsOf: url).components(separatedBy: .whitespacesAndNewlines) else {fatalError()}
+guard let input: [String] = try? String(contentsOf: url).components(separatedBy: .whitespacesAndNewlines).dropLast() else {fatalError()}
 
-enum SeatState: Character {
-	case occupied = "#", empty = "L", floor = ".", padding = " "
-	static func toggle(_ seatState: SeatState) -> SeatState {
-		if seatState == .occupied {return .empty }
-		if seatState == .empty {return .occupied }
-		return seatState
-	}
-}
+input
 
-// create seatmap with padding
-func createSeatMapWithPadding() -> [[Character]]{
-	var seatMap = input.compactMap { string -> [Character]? in
-		if !string.isEmpty {
-			let newString = " " + string + " "
-			return Array(newString)
-		}
-		return nil
-	}
-	let inputColumns = seatMap[0].count
-	let padding = Array(repeating: Character(" "), count: inputColumns)
-	seatMap.insert(padding, at: 0)
-	seatMap.append(padding)
-	return seatMap
-}
-
-func checkAdjacentsAreOccupied(row i: Int, col k: Int, partTwo: Bool ) -> Int {
-	var adjacents = 0
-	let directions: [(x: Int, y: Int)] = [(x: -1,y: -1),(x: 0, y: -1),(x: 1,y: -1),(x: -1,y: 0),(x: 1,y: 0),(x: -1,y: 1),(x: 0, y: 1),(x: 1,y: 1)]
-	if partTwo {
-		for direction in directions {
-			var xOffset = direction.x; var yOffset = direction.y
-			var step = 0
-			while true {
-				step += 1
-				xOffset = step * direction.x ; yOffset = step * direction.y
-				if seatMap[i+yOffset][k+xOffset] == SeatState.padding.rawValue {break}
-				if seatMap[i+yOffset][k+xOffset] == SeatState.floor.rawValue {}
-				if seatMap[i+yOffset][k+xOffset] == SeatState.empty.rawValue {break}
-				if seatMap[i+yOffset][k+xOffset] == SeatState.occupied.rawValue {adjacents += 1; break}
+enum Direction: Int, CaseIterable {
+	case east, south, west, north
+	static var aHead = Direction.east
+	static func turn(_ turning: String, degrees: Int){
+		if turning == "left" {
+			if let currentDirection = allCases.firstIndex(of: aHead) {
+				let idx = (currentDirection + allCases.count - (degrees / 90)) % allCases.count
+				aHead = Direction(rawValue: idx)!
+				print(aHead)
 			}
 		}
-		return adjacents
-	} else {
-	if seatMap[i-1][k-1] == SeatState.occupied.rawValue { adjacents += 1 }
-	if seatMap[i-1][k] == SeatState.occupied.rawValue {adjacents += 1 }
-	if seatMap[i-1][k+1] == SeatState.occupied.rawValue {adjacents += 1 }
-	if seatMap[i][k-1] == SeatState.occupied.rawValue {adjacents += 1 }
-	if seatMap[i][k+1] == SeatState.occupied.rawValue {adjacents += 1 }
-	if seatMap[i+1][k-1] == SeatState.occupied.rawValue {adjacents += 1 }
-	if seatMap[i+1][k] == SeatState.occupied.rawValue {adjacents += 1 }
-	if seatMap[i+1][k+1] == SeatState.occupied.rawValue {adjacents += 1 }
-	return adjacents
+		if turning == "right" {
+			if let currentDirection = allCases.firstIndex(of: aHead) {
+			let idx = (currentDirection + allCases.count + (degrees / 90)) % allCases.count
+			   aHead = Direction(rawValue: idx)!
+			   print(aHead)
+		   }}
 	}
 }
 
-func oneSeatingShuffle(_ seatMap: [[Character]], with currentSeat: SeatState, partTwo: Bool = false ) ->  (nextMap: [[Character]], isSameState: Bool, occupiedSeats: Int) {
-	let maxColumns = seatMap[0].count; let maxRows = seatMap.count
-	var nextSeatMap = seatMap; var occupiedSeats = 0
-	var maxVisibleOccupiedSeats: Int = 4; if partTwo { maxVisibleOccupiedSeats = 5}
-	for i in 1..<maxRows - 1 {
-		for k in 1..<maxColumns - 1 {
-			if seatMap[i][k] == SeatState.occupied.rawValue {occupiedSeats += 1 }
-			if ". ".contains(seatMap[i][k]) {continue}
-			let adjacents = checkAdjacentsAreOccupied(row: i, col: k, partTwo: partTwo)
-			if currentSeat == SeatState.empty {
-				if adjacents == 0 {	nextSeatMap[i][k] = SeatState.occupied.rawValue }
-			} else if currentSeat == SeatState.occupied {
-				if adjacents >= maxVisibleOccupiedSeats  { nextSeatMap[i][k] = SeatState.empty.rawValue }
-			}
-		}
-	}
-	for map in nextSeatMap {
-		print(map.map { String($0)}.joined())}
-			//print("same than last? ",seatMap == nextSeatMap, "occupiedSeats", occupiedSeats)
-	return (nextMap: nextSeatMap, isSameState: seatMap == nextSeatMap, occupiedSeats: occupiedSeats)
+struct Position {
+	var x: Int = 0; var y: Int = 0
+
 }
 
-var isSame = false;	var seatState = SeatState.empty; var occupiedSeats = 0
-var seatMap:[[Character]] = createSeatMapWithPadding()
+print(Direction.allCases)
+Direction.turn("left", degrees: 90)
+Direction.turn("right", degrees: 90)
 
-// part 1 --
-while isSame == false {
-	(seatMap, isSame, occupiedSeats) = oneSeatingShuffle(seatMap, with: seatState)
-	seatState = SeatState.toggle(seatState)
+var directionToHead = Direction.east
+Direction.aHead = .south
+switch Direction.aHead {
+case .north:
+	print("Lots of planets have a north")
+case .south:
+	print("Watch out for penguins")
+case .east:
+	print("Where the sun rises")
+case .west:
+	print("Where the skies are blue")
 }
-let solution1 = occupiedSeats
 
-// part two
-isSame = false;	seatState = SeatState.empty; occupiedSeats = 0
-seatMap = createSeatMapWithPadding()
 
-while isSame == false {
-	(seatMap, isSame, occupiedSeats) = oneSeatingShuffle(seatMap, with: seatState, partTwo: true)
-	seatState = SeatState.toggle(seatState)
-}
-print("Solution part 1: ", solution1) // 2354
-print("Solution part 2: ", occupiedSeats) //2072
+
+//print("Solution part 1: ", solution1) //
+//print("Solution part 2: ", solution2) //
