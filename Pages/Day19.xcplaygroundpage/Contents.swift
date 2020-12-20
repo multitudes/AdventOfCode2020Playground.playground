8a96da35e0bed42930e9c63484053f7fd14b43be
laurent <wrmultitudes@gmail.com>
2020-12-20 16:03:37 +0100

    Update Contents.swift

--- Pages/Day19.xcplaygroundpage/Contents.swift
+++ Pages/Day19.xcplaygroundpage/Contents.swift
@@ -5,33 +5,25 @@ import Foundation
 guard let url = Bundle.main.url(forResource: "input", withExtension: "txt") else { fatalError()}
 //guard let url = Bundle.main.url(forResource: "Day19-example", withExtension: "txt") else {fatalError()}
 guard let inputFile = try? String(contentsOf: url).lines else {fatalError()}
+
 let input = inputFile.split {$0 == "" }
 let rules = input[0]
 let messages = input[1]
-print(rules)
-print(messages)
 var rulesDict: [Int: String] = [:]
 
 for rule in rules {
 	if let regex = rule.getCapturedGroupsFrom(regexPattern: "(\\d+):[ \"]{1,2}(\\w)[\"]{1}") {
-		print("base")
 		let key = Int(regex[0])!
 		let expr = " " + regex[1] + " "
-		print(key, expr)
 		rulesDict[key] = expr
 	} else if let regex = rule.getCapturedGroupsFrom(regexPattern: "(\\d+):(.+)") {
 		let key = Int(regex[0])!
 		var expr = regex[1]
 		expr = ("(" + expr + " )")//.replacingOccurrences(of: " ", with: "")
-		print(key, expr)
 		rulesDict[key] = expr
 	}
 }
-//rulesDict.description
-rulesDict[5]
-var ruleZero = (rulesDict[0] ?? "")//.replacingOccurrences(of: " ", with: "")
-print("\n-----------\n")
-//ruleZero = "( a ( 2 3 | 3 2 ) b )"
+
 func createRegex(from ruleZero: String) -> String {
 	let arrayRules = ruleZero.split(separator: " ").map {String($0)}
 	var regexPattern = arrayRules
@@ -43,32 +35,19 @@ func createRegex(from ruleZero: String) -> String {
 			regexPattern[idx] = itemToResolve
 		}
 	}
-	print(regexPattern)
 	return regexPattern.joined(separator: " ")
 }
 
-var regexPattern = ruleZero
+var regexPattern = (rulesDict[0] ?? "")
 while !regexPattern.allSatisfy({"ab()| ".contains($0)}) {
-
-regexPattern = createRegex(from: regexPattern)
-//regexPattern = createRegex(from: regexPattern)
-//regexPattern = createRegex(from: regexPattern)
+	regexPattern = createRegex(from: regexPattern)
 }
 print(regexPattern)
 regexPattern = "^" + regexPattern.replacingOccurrences(of:" ", with: "") + "$"
-print("regexPattern", regexPattern)
 guard let regex = try? NSRegularExpression(pattern: regexPattern) else { fatalError("invalid regex expression \n") }
-
 var count = 0
 for message in messages {
 	let range = NSRange(location: 0, length: message.utf16.count)
 	if regex.firstMatch(in: message, options: [], range: range) != nil { count += 1}
-	//if message.matches(regex: regexPattern){count += 1}
 }
-count
-//
-//"ababbb".matches(regex: regexPattern)
-//"bababa".matches(regex: regexPattern)
-//"abbbab".matches(regex: regexPattern)
-//"aaabbb".matches(regex: regexPattern)
-//"aaaabbb".matches(regex: regexPattern)
+print("Solution part one : ", count)
