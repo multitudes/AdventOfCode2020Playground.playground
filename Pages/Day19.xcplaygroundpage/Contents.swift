1ae6cf496973f9b9dce09ecfb1f30ce01d63553f
laurent <wrmultitudes@gmail.com>
2020-12-20 21:29:18 +0100

    day19

--- Pages/Day19.xcplaygroundpage/Contents.swift
+++ Pages/Day19.xcplaygroundpage/Contents.swift
@@ -2,8 +2,9 @@ import Foundation
 
 // --- Day 19: Monster Messages ---
 
-guard let url = Bundle.main.url(forResource: "input", withExtension: "txt") else { fatalError()}
-//guard let url = Bundle.main.url(forResource: "Day19-example", withExtension: "txt") else {fatalError()}
+guard let url = Bundle.main.url(forResource: "input2", withExtension: "txt") else { fatalError()}
+//guard let url = Bundle.main.url(forResource: "Day19-example2", withExtension: "txt") else {fatalError()}
+//guard let url = Bundle.main.url(forResource: "Day19-example2updated", withExtension: "txt") else {fatalError()}
 guard let inputFile = try? String(contentsOf: url).lines else {fatalError()}
 
 let input = inputFile.split {$0 == "" }
@@ -16,33 +17,82 @@ for rule in rules {
 		let key = Int(regex[0])!
 		let expr = " " + regex[1] + " "
 		rulesDict[key] = expr
+		print("key, expr", key, expr)
 	} else if let regex = rule.getCapturedGroupsFrom(regexPattern: "(\\d+):(.+)") {
 		let key = Int(regex[0])!
 		var expr = regex[1]
-		expr = ("(" + expr + " )")//.replacingOccurrences(of: " ", with: "")
+		expr = ("(" + expr + " )")
+		print("key, expr", key, expr)
 		rulesDict[key] = expr
 	}
 }
-
+var resolvedDict: [Int: Int] = [:]
 func createRegex(from ruleZero: String) -> String {
 	let arrayRules = ruleZero.split(separator: " ").map {String($0)}
 	var regexPattern = arrayRules
 	for (idx,rule) in arrayRules.enumerated() {
 		if rule == "(" || rule == ")" || rule == "a" || rule == "b" || rule == "|" {continue}
 		print("rules",rule)
-		if let itemToResolve = rulesDict[Int(String(rule))!] {
+		let keyRule = Int(String(rule))!
+		print("keyRule",keyRule)
+		if let itemToResolve = rulesDict[keyRule] {
 			print(itemToResolve)
-			regexPattern[idx] = itemToResolve
+			if itemToResolve == " a " || itemToResolve == " b " {
+				regexPattern[idx] = itemToResolve
+				continue
+			} else if resolvedDict[keyRule, default: 0] < 200 {
+				print("resolvedDict[\(keyRule), default: 0] ", resolvedDict[keyRule, default: 0] )
+				resolvedDict[keyRule, default: 0] += 1
+				regexPattern[idx] = itemToResolve
+			} else {
+				print("skipped     ------ ", keyRule)
+			}
 		}
 	}
 	return regexPattern.joined(separator: " ")
 }
 
 var regexPattern = (rulesDict[0] ?? "")
-while !regexPattern.allSatisfy({"ab()| ".contains($0)}) {
-	regexPattern = createRegex(from: regexPattern)
-}
+//while !regexPattern.allSatisfy({"ab()| ".contains($0)}) {
+regexPattern = createRegex(from: regexPattern)
 print(regexPattern)
+regexPattern = createRegex(from: regexPattern)
+print(regexPattern)
+regexPattern = createRegex(from: regexPattern)
+print(regexPattern)
+regexPattern = createRegex(from: regexPattern)
+
+print(regexPattern)
+regexPattern = createRegex(from: regexPattern)
+
+print(regexPattern)
+regexPattern = createRegex(from: regexPattern)
+print(regexPattern)
+regexPattern = createRegex(from: regexPattern)
+
+print(regexPattern)
+regexPattern = createRegex(from: regexPattern)
+print(regexPattern)
+regexPattern = createRegex(from: regexPattern)
+
+print(regexPattern)
+regexPattern = createRegex(from: regexPattern)
+print(regexPattern)
+regexPattern = createRegex(from: regexPattern)
+
+print(regexPattern)
+regexPattern = createRegex(from: regexPattern)
+
+print(regexPattern)
+regexPattern = createRegex(from: regexPattern)
+
+print(regexPattern)
+regexPattern = createRegex(from: regexPattern)
+
+//}
+print(regexPattern)
+
+
 regexPattern = "^" + regexPattern.replacingOccurrences(of:" ", with: "") + "$"
 guard let regex = try? NSRegularExpression(pattern: regexPattern) else { fatalError("invalid regex expression \n") }
 var count = 0
