4de46ecb5f73d78798d0cbdae62e231f135952fb
laurent <wrmultitudes@gmail.com>
2020-12-22 10:45:08 +0100

    day21

--- Pages/Day21.xcplaygroundpage/Contents.swift
+++ Pages/Day21.xcplaygroundpage/Contents.swift
@@ -3,12 +3,15 @@ import Foundation
 // --- Day 21: Allergen Assessment ---
 
 //guard let url = Bundle.main.url(forResource: "input", withExtension: "txt") else { fatalError()}
-guard let url = Bundle.main.url(forResource: "Day20-example", withExtension: "txt") else {fatalError()}
+guard let url = Bundle.main.url(forResource: "Day21-example", withExtension: "txt") else {fatalError()}
 
-guard let inputFile = try? String(contentsOf: url).lines else {fatalError()}
-var input = inputFile.split {$0 == "" }.map {Array($0) }
-input.count
-print(input)
+guard let inputFile = try? String(contentsOf: url).replacingOccurrences(of: ")", with: "").linesNotEmpty else {fatalError()}
+var input = inputFile.map { $0.split(separator: " ").map {String($0)}.split { $0 == "(contains"}}
+	.map { return (ingredients: Set($0[0]),allergens: Set($0[1]) ) }
+input
+print(input.description)
+
+
 
 
 
---
+++ Pages/Day22.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+// --- Day 21: Allergen Assessment ---
+
+//guard let url = Bundle.main.url(forResource: "input", withExtension: "txt") else { fatalError()}
+guard let url = Bundle.main.url(forResource: "Day21-example", withExtension: "txt") else {fatalError()}
+
+guard let inputFile = try? String(contentsOf: url).lines else {fatalError()}
+var input = inputFile //.split {$0 == "" }.map {Array($0) }
+input.count
+print(input)
+
+
+
+
+print("Solution part one : ")
