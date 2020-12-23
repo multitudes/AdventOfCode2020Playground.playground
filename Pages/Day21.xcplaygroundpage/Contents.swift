e031951d89ab8e32a27e0b168cd6f705113e52d9
laurent <wrmultitudes@gmail.com>
2020-12-23 18:16:03 +0100

    Update Contents.swift

--- Pages/Day21.xcplaygroundpage/Contents.swift
+++ Pages/Day21.xcplaygroundpage/Contents.swift
@@ -6,7 +6,7 @@ guard let url = Bundle.main.url(forResource: "input", withExtension: "txt") else
 //guard let url = Bundle.main.url(forResource: "Day21-example", withExtension: "txt") else {fatalError()}
 
 guard let inputFile = try? String(contentsOf: url).replacingOccurrences(of: "[,)]", with: "", options: .regularExpression).linesNotEmpty else {fatalError()}
-var input = inputFile.map { $0.split(separator: " ").map {String($0) }
+var input: [(ingredients: Set<String>, allergens: Set<String>)] = inputFile.map { $0.split(separator: " ").map {String($0) }
 	.split { $0 == "(contains"}}
 	.map { return (ingredients: Set($0[0]),allergens: Set($0[1]) ) }
 input.sort {$0.allergens.count < $1.allergens.count}
