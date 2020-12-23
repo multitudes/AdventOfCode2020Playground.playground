f20276c661858eb784da161804625fa8eb1136c3
laurent <wrmultitudes@gmail.com>
2020-12-23 21:26:31 +0100

    day22

--- Pages/Day22.xcplaygroundpage/Contents.swift
+++ Pages/Day22.xcplaygroundpage/Contents.swift
@@ -3,14 +3,31 @@ import Foundation
 // --- Day 22: Crab Combat ---
 
 //guard let url = Bundle.main.url(forResource: "input", withExtension: "txt") else { fatalError()}
-guard let url = Bundle.main.url(forResource: "Day21-example", withExtension: "txt") else {fatalError()}
+guard let url = Bundle.main.url(forResource: "Day22-example", withExtension: "txt") else {fatalError()}
 
 guard let inputFile = try? String(contentsOf: url).lines else {fatalError()}
-var input = inputFile //.split {$0 == "" }.map {Array($0) }
+var input = inputFile.split {$0 == "" }.map {Array($0) }
 input.count
 print(input)
 
+// init
+var one = input[0].compactMap {Int($0)}
+var two = input[1].compactMap {Int($0)}
 
+while !one.isEmpty && !two.isEmpty {
+	func playRound() {
+		let topOne = one.remove(at: 0); let topTwo = two.remove(at: 0)
+		if topOne > topTwo {
+			one.append(contentsOf: [topOne, topTwo])
+		} else {
+			two.append(contentsOf: [topTwo, topOne])
+		}
+	}
+	playRound()
+}
+
+one
+two
 
 
 print("Solution part one : ")
